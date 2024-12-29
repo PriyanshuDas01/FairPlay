f86d9a776e72e527bac969a40e9313dda1bf4d9e
PriyanshuDas01 <priyanshudas01@gmail.com>
2024-12-29 13:44:11 +0530

    mongodb connected

--- gopage/main.go
+++ gopage/main.go
@@ -1,97 +1,150 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Task struct {
-	ID        int    `json:"id"`
-	Completed bool   `json:"completed"`
-	Body      string `json:"body"`
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"` //omitempty to remove teh null id value 000...
+	Completed bool               `json:"completed"`
+	Body      string             `json:"body"`
 }
 
+var collection *mongo.Collection
+
 func main() {
-	fmt.Println("Hello")
-	app := fiber.New()
+	fmt.Println("hello world")
 
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatal("Error loading the env!")
+	if os.Getenv("ENV") != "production" {
+		// Load the .env file if not in production
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file:", err)
+		}
 	}
 
-	PORT := os.Getenv("PORT")
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client, err := mongo.Connect(context.Background(), clientOptions)
 
-	tasks := []Task{}
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Root
-	app.Get("/api/tasks", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(tasks)
-	})
+	defer client.Disconnect(context.Background())
 
-	app.Post("/api/tasks", func(c *fiber.Ctx) error {
-		newTask := new(Task)
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		if err := c.BodyParser(newTask); err != nil {
-			return c.Status(400).JSON(fiber.Map{"error": "Invalid request body"})
+	fmt.Println("Connected to MONGODB ATLAS")
+
+	collection = client.Database("golang_db").Collection("tasks")
+
+	app := fiber.New()
+
+	app.Get("/api/tasks", getTasks)
+	app.Post("/api/tasks", createTask)
+	app.Patch("/api/tasks/:id", updateTask)
+	app.Delete("/api/tasks/:id", deleteTask)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	if os.Getenv("ENV") == "production" {
+		app.Static("/", "./client/dist")
+	}
+
+	log.Fatal(app.Listen("0.0.0.0:" + port))
+}
+
+func getTasks(c *fiber.Ctx) error {
+	var tasks []Task
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return err
+	}
+
+	defer cursor.Close(context.Background())
+
+	for cursor.Next(context.Background()) {
+		var task Task
+		if err := cursor.Decode(&task); err != nil {
+			return err
 		}
+		tasks = append(tasks, task)
+	}
 
-		if newTask.Body == "" {
-			return c.Status(400).JSON(fiber.Map{"error": "Task body can't be empty"})
-		}
+	return c.JSON(tasks)
+}
 
-		newTask.ID = len(tasks) + 1
-		tasks = append(tasks, *newTask)
+func createTask(c *fiber.Ctx) error {
+	task := new(Task)
 
-		return c.Status(201).JSON(newTask)
-	})
+	if err := c.BodyParser(task); err != nil {
+		return err
+	}
 
-	// GET route to retrieve all tasks
-	app.Get("/api/tasks", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(tasks)
-	})
+	if task.Body == "" {
+		return c.Status(400).JSON(fiber.Map{"error": "Task body cannot be empty"})
+	}
 
-	// PATCH route to update a task
-	app.Patch("/api/tasks/:id", func(c *fiber.Ctx) error {
-		id, err := strconv.Atoi(c.Params("id"))
-		if err != nil {
-			return c.Status(400).JSON(fiber.Map{"error": "Invalid task ID"})
-		}
+	insertResult, err := collection.InsertOne(context.Background(), task)
+	if err != nil {
+		return err
+	}
 
-		// Find and update the task
-		for i := range tasks {
-			if tasks[i].ID == id {
-				tasks[i].Completed = true
-				return c.Status(200).JSON(tasks[i])
-			}
-		}
+	task.ID = insertResult.InsertedID.(primitive.ObjectID)
 
-		return c.Status(404).JSON(fiber.Map{"error": "Task not found"})
-	})
+	return c.Status(201).JSON(task)
+}
 
-	app.Delete("/api/tasks/:id", func(c *fiber.Ctx) error {
-		id, err := strconv.Atoi(c.Params("id"))
-		if err != nil {
-			return c.Status(400).JSON(fiber.Map{"error": "Invalid task ID"})
-		}
+func updateTask(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
 
-		// Find and delete the task
-		for i, task := range tasks {
-			if task.ID == id {
-				// Remove the task from the slice
-				tasks = append(tasks[:i], tasks[i+1:]...)
-				return c.Status(200).JSON(fiber.Map{"message": "Task deleted successfully"})
-			}
-		}
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid task ID"})
+	}
 
-		return c.Status(404).JSON(fiber.Map{"error": "Task not found"})
-	})
+	filter := bson.M{"_id": objectID}
+	update := bson.M{"$set": bson.M{"completed": true}}
 
-	log.Fatal(app.Listen(":" + PORT))
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
+}
+
+func deleteTask(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid task ID"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	_, err = collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
 }
